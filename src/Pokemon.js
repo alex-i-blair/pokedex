2cbcbb072f8694406334cdc554874614ec768165
Alex Blair <bclan11@gmail.com>
2022-02-11 14:28:14 -0800

    logic fixed around the toTitle util

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,22 +1,11 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { toTitle } from './utils';
 
 export default function Pokemon({ poke }) {
-  const [name, setName] = useState('');
-
-  useEffect(() => {
-    async function onLoad() {
-      const data = await toTitle(poke.pokemon);
-      setName(data);
-    }
-    onLoad();
-  }, []);
-
   return (
     <Link to={`/pokemon/${poke.id}`}>
       <div className="poke">
-        {name}
+        {toTitle(poke.pokemon)}
         <img className="poke-img" src={poke.url_image} />
       </div>
     </Link>
--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -19,18 +19,68 @@ export default function PokemonDetail() {
     <>
       <Link to="/">Home</Link>
       <section className="detail-container">
-        <div className="detail-card">
-          <p>{pokemon.pokemon && toTitle(pokemon.pokemon)}</p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-        </div>
+        {pokemon.pokemon && (
+          <div className="detail-card">
+            <h1>{toTitle(pokemon.pokemon)}</h1>
+            <img src={pokemon.url_image}></img>
+            <div className="stat-container">
+              <h2>Type:</h2>
+              <p>{toTitle(pokemon.type_1)}</p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+            <div>
+              <h2></h2>
+              <p></p>
+            </div>
+          </div>
+        )}
       </section>
     </>
   );
