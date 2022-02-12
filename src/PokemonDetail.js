16617ef533595307eb6420fb40f80f62f9d71687
Alex Blair <bclan11@gmail.com>
2022-02-11 17:31:51 -0800

    done??

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -15,69 +15,83 @@ export default function PokemonDetail() {
     onLoad();
   }, [params.id]);
 
+  function handlePokeClick() {
+    window.open(pokemon.pokedex);
+  }
+
   return (
     <>
-      <Link to="/">Home</Link>
+      <div className="nav-home">
+        <Link to="/">Home</Link>
+      </div>
       <section className="detail-container">
         {pokemon.pokemon && (
           <div className="detail-card">
-            <h1>{toTitle(pokemon.pokemon)}</h1>
-            <img src={pokemon.url_image}></img>
-            <div className="stat-container">
-              <h2>Type:</h2>
-              <p>{toTitle(pokemon.type_1)}</p>
+            <div className="detail-top">
+              <h1>{toTitle(pokemon.pokemon)}</h1>
+              <img src={pokemon.url_image}></img>
             </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
-            </div>
-            <div>
-              <h2></h2>
-              <p></p>
+            <div className="detail-bottom">
+              <div className="stat-container">
+                <h4>Type 1:</h4>
+                <p>{toTitle(pokemon.type_1)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Type 2:</h4>
+                <p>{toTitle(pokemon.type_2)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Shape:</h4>
+                <p>{toTitle(pokemon.shape)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Ability 1:</h4>
+                <p>{toTitle(pokemon.ability_1)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Ability 2:</h4>
+                <p>{toTitle(pokemon.ability_2)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Hidden Ability:</h4>
+                <p>{toTitle(pokemon.ability_hidden)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Egg Group 1:</h4>
+                <p>{toTitle(pokemon.egg_group_1)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Egg Group 2:</h4>
+                <p>{toTitle(pokemon.egg_group_2)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Shape:</h4>
+                <p>{toTitle(pokemon.shape)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Pokebase:</h4>
+                <p>{toTitle(pokemon.pokebase)}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Height:</h4>
+                <p>{pokemon.height}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Weight:</h4>
+                <p>{pokemon.weight}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Attack:</h4>
+                <p>{pokemon.attack}</p>
+              </div>
+              <div className="stat-container">
+                <h4>Defense:</h4>
+                <p>{pokemon.defense}</p>
+              </div>
+              <div className="stat-container">
+                <h4>HP:</h4>
+                <p>{pokemon.hp}</p>
+              </div>
             </div>
           </div>
         )}
